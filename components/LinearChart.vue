55ebda500aa1d6a2499566d522a624e21563857e
Olga Kashapova <invispo16@gmail.com>
2024-07-05 12:25:58 +0300

    Revert "Revert "Revert "added charts"""
    This reverts commit aa402b3ae7d50fabc11f8ffca17841f8129d364e.

--- components/LinearChart.vue
+++ components/LinearChart.vue
@@ -13,82 +13,29 @@ import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, L
 ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement);
 
 interface Props {
+  labels: string[],
   data: number[],
   label: string,
-  altLabels?: boolean,
+  color: string
 }
 
 const props = defineProps<Props>();
 
-const monthDays = () => {
-  let arr = [];
-  for (let i = 0; i < 8; i++) {
-    arr[i] = `${i}`;
-  }
-  return arr;
-}
-
-function randomInteger(min: number, max: number) {
-  let rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
-};
-
 const chartData = reactive({
-  labels: monthDays(),
+  labels: props.labels,
   datasets: [{
     data: props.data,
     label: props.label,
-    borderColor: `rgb(${randomInteger(0, 256)}, ${randomInteger(0, 256)}, ${randomInteger(0, 256)})`,
+    borderColor: 'rgb(75, 192, 192)',
     tension: 0.1
   }]
 });
 
 const chartOptions = reactive({
-
-  scales: {
-    y: {
-      ticks: {
-        font: {
-          family:	"Ubuntu Sans, sans-serif"
-        },
-        callback: (value) => {
-          if (props.altLabels) {
-            switch (value) {
-              case 0:
-                return 'нет';
-              case 1:
-                return 'может быть';
-              case 2:
-                return 'часто';
-              case 3:
-                return 'да';
-              default:
-                return ''
-            }
-          }
-          else {
-            return value;
-          }
-        }
-      },
-    }
-  },
-  plugins: {
-    legend: {
-      labels: {
-        font: {
-          family:	"Ubuntu Sans, sans-serif"
-        }
-      }
-    },
-    tooltip: {
-      titleFont: {
-          family:	"Ubuntu Sans, sans-serif"
-      },
-      bodyFont: {
-          family:	"Ubuntu Sans, sans-serif"
-      },
-    }
-  }
+  responsive: true
 });
 </script>
+
+<style scoped>
+
+</style>
